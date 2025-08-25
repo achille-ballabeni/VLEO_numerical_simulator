06aa5011fb8a55c02aab924c3f689a6937ffb70c
achille-ballabeni <114033785+achille-ballabeni@users.noreply.github.com>
2025-08-25 18:44:48 +0200

    Integrate simulink position with satellite viewer

--- src/LOS_model_validation.m
+++ src/LOS_model_validation.m
@@ -1,20 +1,41 @@
-%% Orbital parameters
+%% Earth constants
+Re = earthRadius; % [m]
+mi = 398600.418e9; % [m^3/s^2]
+
+%% Orbital parameters and initial conditions
+a = Re + 500e3; % [m]
 e = 0;
-Re = 6378; % [km]
-a = Re + 500;
-mi = 398600.418; % [km^3/s^2] 
+inclination = 0;
+raan = 0; % Right Ascension of Ascending node [deg]
+aop = 0; % Arguement of Pericenter [deg]
+ta = 0; % True amomaly [deg]
+
+q0 = [1,0,0,0];
+w0 = [0,0,0];
 T = 2 * pi * sqrt(a^3 / mi); % [s]
+startTime = datetime(2020,1,1,12,0,0);
+startTimeJD = juliandate(startTime);
+simLength = T;
+
+%% Setup simulation
+model = "satellite_propagator.slx";
+simIn = Simulink.SimulationInput(model);
+simIn = simIn.setModelParameter("StopTime", num2str(simLength));
+
+%% Run simulation
+simOut = sim(simIn);
+t = simOut.tout;
 
 %% Define the satellite scenario
 % Set simulation duration to 1 full orbital period
-startTime = datetime(2025,08,24);
-stopTime = startTime + seconds(T);
+
 sampleTime = 60;
+stopTime = startTime + seconds(simLength);
 sc = satelliteScenario(startTime,stopTime,sampleTime);
 
 % Add satellite
-a = a*1000;
-sat = satellite(sc,a,e,0,0,0,0);
+Rsat = simOut.yout{1}.Values;
+sat = satellite(sc,Rsat);
 sat.Visual3DModel = "bus.glb";
 coordinateAxes(sat);
 
