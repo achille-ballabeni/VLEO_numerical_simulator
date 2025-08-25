3ea9c223e9eb6ebd29511db74974b588c26709d4
achille-ballabeni <114033785+achille-ballabeni@users.noreply.github.com>
2025-08-25 22:42:53 +0200

    Added angles to quaternion conversion for initial attitude

--- src/LOS_model_validation.m
+++ src/LOS_model_validation.m
@@ -1,18 +1,25 @@
+clear
+
 %% Earth constants
 Re = earthRadius; % [m]
 mi = 398600.418e9; % [m^3/s^2]
 
 %% Orbital parameters and initial conditions
-a = Re + 500e3; % [m]
-e = 0;
-inclination = 0;
+a = Re + 3000e3; % [m]
+e = 0.3;
+inclination = 0; %[deg]
 raan = 0; % Right Ascension of Ascending node [deg]
 aop = 0; % Arguement of Pericenter [deg]
 ta = 0; % True amomaly [deg]
 T = 2 * pi * sqrt(a^3 / mi); % [s]
 
-q0 = [1,0,0,0];
-w0 = [0,0,360/T]; % [deg/s]
+% Define 3 rotation angles wrt to ECI frame
+theta3 = deg2rad(0);
+theta2 = deg2rad(-90);
+theta1 = deg2rad(0);
+
+q0 = angle2quat(theta3,theta2,theta1,"ZYX");
+w0 = [0,0,0]; % Angular velocity in body frame [deg/s]
 startTime = datetime(2020,1,1,12,0,0);
 startTimeJD = juliandate(startTime);
 simLength = T;
@@ -38,7 +45,7 @@ sc = satelliteScenario(startTime,stopTime,sampleTime);
 % Add satellite
 Rsat = simOut.yout{1}.Values;
 Qin2body = simOut.yout{4}.Values;
-sat = satellite(sc,Rsat);
+sat = satellite(sc,Rsat,"Name","CubeSat");
 pointAt(sat,Qin2body);
 sat.Visual3DModel = "bus.glb";
 coordinateAxes(sat);
