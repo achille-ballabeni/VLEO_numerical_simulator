bf6748c1167155033b7c747ce99845256ba8e3b4
achille-ballabeni <114033785+achille-ballabeni@users.noreply.github.com>
2025-08-27 01:05:33 +0200

    Included calculation of LOS intersection

--- src/LOS_model_validation.m
+++ src/LOS_model_validation.m
@@ -1,4 +1,9 @@
 clear
+% TODO: create a class or function to avoid a huge main code.
+% You could have:
+% - One satellite class, that has methods to simulate and then play in the
+% satellite scenario + post processing methods.
+% - A plotting class.
 
 %% Earth constants
 Re = earthRadius; % [m]
@@ -15,11 +20,11 @@ T = 2 * pi * sqrt(a^3 / mi); % [s]
 
 % Define 3 rotation angles wrt to ECI frame
 theta3 = deg2rad(0);
-theta2 = deg2rad(-90);
+theta2 = deg2rad(0);
 theta1 = deg2rad(0);
 
 q0 = angle2quat(theta3,theta2,theta1,"ZYX");
-w0 = [0,0,0]; % Angular velocity in body frame [deg/s]
+w0 = [0,360/T,0]; % Angular velocity in body frame [deg/s]
 startTime = datetime(2020,1,1,12,0,0);
 startTimeJD = juliandate(startTime);
 simLength = T;
@@ -35,16 +40,39 @@ simIn = simIn.setModelParameter("StopTime", num2str(simLength), ...
 simOut = sim(simIn);
 t = simOut.tout;
 
-%% Define the satellite scenario
+%% Do some post processing
+Rsat = simOut.yout{1}.Values;
+Qin2body = simOut.yout{4}.Values;
+% Inverse quaternion to go from body to inertial
+Qbody2in = quatinv(Qin2body.Data);
+% Find direction of line of sight, considered as exiting from the x axis of
+% the satellite.
+LOS_hat = quatrotate(Qbody2in,[-1,0,0]);
+% Intersection between line of sight and earth surface
+rho = -dot(LOS_hat,Rsat.Data,2) - sqrt((dot(LOS_hat,Rsat.Data,2)).^2 - vecnorm(Rsat.Data,2,2).^2 + Re^2);
+rho(imag(rho) ~= 0) = 0;
+rho(rho<0) = 0;
+indexes = rho ~= 0;
+% Find the ground track vector
+Rtar = rho.*LOS_hat;
+Rgt(indexes,:) = Rsat.Data(indexes,:) + Rtar(indexes,:);
+% Quick plot of the orbit to check
+plot3(Rgt(:,1),Rgt(:,2),Rgt(:,3))
+hold on 
+plot3(Rsat.Data(:,1),Rsat.Data(:,2),Rsat.Data(:,3))
+axis equal
+grid on
+
+
+%% Define the satellite scenario for visualization
 % Set simulation duration to 1 full orbital period
+
 
 sampleTime = 60;
 stopTime = startTime + seconds(simLength);
 sc = satelliteScenario(startTime,stopTime,sampleTime);
 
 % Add satellite
-Rsat = simOut.yout{1}.Values;
-Qin2body = simOut.yout{4}.Values;
 sat = satellite(sc,Rsat,"Name","CubeSat");
 pointAt(sat,Qin2body);
 sat.Visual3DModel = "bus.glb";
